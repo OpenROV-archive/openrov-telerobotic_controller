b98413e1cb988778bef75d99ab9c006ca6a7e708
Henry Stewart <henry@Henrys-MacBook-Air.local>
2015-02-19 17:11:52 -0800

    namespacing fix

--- public/js/telerobotic_controller.js
+++ public/js/telerobotic_controller.js
@@ -79,7 +79,7 @@
 
     // when Bistri API client is ready, function
     // "onBistriConferenceReady" is invoked
-
+    bistriCockpit = cockpit;
     onBistriConferenceReady = function () {
 
         // test if the browser is WebRTC compatible
@@ -273,7 +273,7 @@
         container.scrollTop = container.scrollHeight;
       console.log("new msg received...should be emitting event");
       //.emit('rovpilot.setYaw', 0);
-      cockpit.socket.emit("escs_poweron");
+      bistriCockpit.socket.emit("escs_poweron");
     }
 
     // when checking for chat partner presence
