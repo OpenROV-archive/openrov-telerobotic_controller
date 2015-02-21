95dee5d571fd4b1971435165f18a2a8164e91d86
Henry Stewart <henry@Henrys-MacBook-Air.local>
2015-02-20 17:05:43 -0800

    sec_poweron

--- public/js/telerobotic_controller.js
+++ public/js/telerobotic_controller.js
@@ -273,7 +273,7 @@
         container.scrollTop = container.scrollHeight;
       console.log("new msg received...should be emitting event");
       //.emit('rovpilot.setYaw', 0);
-      // self.socket.emit("escs_poweron");
+      self.socket.emit("escs_poweron");
       self.socket.emit("rovpilot.toggleLights");
       self.socket.emit("rovPilot.laserToggle");
     }
