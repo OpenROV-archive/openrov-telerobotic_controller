624b0ea7bd5add7b24aa9bc7105b6eef5f3f180a
Henry Stewart <henry@Henrys-MacBook-Air.local>
2015-05-07 13:43:40 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function telerobotic_controller(name, deps) {
+    var cp = require('child_process'); 
+    var serialNumber;
+    var serialProc = cp.spawn('bash', ['/opt/openrov/cockpit/linux/bb-show-serial.sh']);
+    serialProc.stdout.on('data', function(data){
+        serialNumber = data.toString();
+        var parts = serialNumber.split(':');
+        if (parts.length > 0) {
+          serialNumber = parts[parts.length -1].trim();
+      }
+    });
     console.log("This is where plugin code for telerobotic_controller loads in the node process.");
-
+    
   //This is how you would register a listner to traffic from the browser
   /*
   deps.io.sockets.on('connection', function (socket) {
